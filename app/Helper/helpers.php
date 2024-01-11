b2da84453b252ca744ad0e756f17c2ca29299d70
Samuel1724 <jsamuel24577@gmail.com>
2024-01-12 02:35:28 +0530

    uploading all

---
+++ app/Helper/helpers.php
@@ -0,0 +1,58 @@
+<?php
+
+
+/** Handle File Uploads */
+
+function handleUpload($inputName, $model=null){
+    try{
+        if(request()->hasFile($inputName)){
+            if($model && \File::exists(public_path($model->{$inputName}))){
+                \File::delete(public_path($model->{$inputName}));
+            }
+            $file = request()->file($inputName);
+            $fileName = rand() . $file->getClientOriginalName();
+            $file->move(public_path('/uploads'), $fileName);
+    
+            $filePath = "/uploads/" . $fileName;
+    
+            return $filePath;
+    
+        }
+    }catch(\Exception $e){
+        throw $e;
+    }
+   
+}
+
+/** Handle File Deletions */
+
+function deleteFileIfExist($filePath)
+{   
+    try{
+        if(\File::exists(public_path($filePath))){
+            \File::delete(public_path($filePath));
+        }
+    }catch(\Exception $e){
+        throw $e;
+    }
+}
+
+/** Handle Dynamic Get Colours */
+
+function getColor($index){
+    $colors = ['#558bff', '#fecc90', '#ff885e', '#282828', '#190844', '#9dd3ff'];
+
+    return $colors[$index % count($colors)];
+}
+
+/** Set Sidebar Active */
+
+function setSidebarActive($route){
+    if(is_array($route)){
+        foreach($route as $r){
+            if(request()->routeIs($r)){
+                return 'active';
+            }
+        }
+    }
+}
---
+++ app/Http/Controllers/Admin/BlogController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\DataTables\BlogDataTable;
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\BlogCategory;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(BlogDataTable $dataTable)
+    {
+        return $dataTable->render('admin.blog.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $categories = BlogCategory::all();
+        return view('admin.blog.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'image' => ['required', 'max:5000', 'image'],
+            'title' => ['required', 'max:100'],
+            'category' => ['required','numeric'],
+            'description' => ['required']
+        ]);
+
+        $imagePath = handleUpload('image');
+
+        $blog = new Blog();
+        $blog->image = $imagePath;
+        $blog->title = $request->title;
+        $blog->category = $request->category;
+        $blog->description = $request->description;
+        $blog->save();
+
+        toastr()->success('Blog Created Successfully !');
+        return redirect()->route('admin.blog.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $categories = BlogCategory::all();
+        $blog = Blog::findOrFail($id);
+        return view('admin.blog.edit', compact('categories', 'blog'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'image' => ['max:5000', 'image'],
+            'title' => ['required', 'max:100'],
+            'category' => ['required','numeric'],
+            'description' => ['required']
+        ]);
+
+        $blog = Blog::findOrFail($id);
+        $imagePath = handleUpload('image', $blog);
+
+        $blog->image = (!empty($imagePath) ? $imagePath : $blog->image);
+        $blog->title = $request->title;
+        $blog->category = $request->category;
+        $blog->description = $request->description;
+        $blog->update();
+
+        toastr()->success('Blog Updated Successfully !');
+        return redirect()->route('admin.blog.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $blog = Blog::findOrFail($id);
+        deleteFileIfExist($blog->name);
+        $blog->delete();
+    }
+}
